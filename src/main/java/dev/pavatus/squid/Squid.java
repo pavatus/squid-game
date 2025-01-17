21e37264c514b5a13f2b57bbe581436bf2efe61b
James Hall <james.hall01@outlook.com>
2025-01-17 20:08:58 +0000

    fmt

--- src/main/java/dev/pavatus/squid/Squid.java
+++ src/main/java/dev/pavatus/squid/Squid.java
@@ -1,16 +1,15 @@
 package dev.pavatus.squid;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Squid implements ModInitializer {
-	public static final String MOD_ID = "squid";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "squid";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
+    @Override
+    public void onInitialize() {
 
-	}
+    }
 }
